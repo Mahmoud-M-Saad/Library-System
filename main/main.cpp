0378cfc50f948cf552adc46bf77ecc7a5ec369c7
Mahmoud Mohamed <mahmoudsaad6675@gmail.com>
2024-09-05 15:31:26 +0300

    Add project files.

---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/book.h"
+using namespace std;
+
+int main() {
+	test();
+}
---
+++ src/book.cpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "../include/book.h"
+
+void test() {
+	cout << "WOOOOOOOOOOOOOOOOOW";
+}
